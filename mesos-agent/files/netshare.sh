9554c73dfd765abf42ddf25d74595c054f9f546e
Stéphane Cottin <stephane.cottin@vixns.com>
2017-03-14 00:03:30 +0100

    netshare update

--- mesos-agent/files/netshare.sh
+++ mesos-agent/files/netshare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 exec 1> >(exec logger -p user.info -t "$(basename $(pwd))[$(cat $(pwd)/supervise/pid)]")
 exec 2> >(exec logger -p user.err -t "$(basename $(pwd))[$(cat $(pwd)/supervise/pid)]")
-exec /usr/bin/docker-volume-netshare --basedir=/srv nfs -o port=2049,nolock,proto=tcp --version=3
+exec /usr/bin/docker-volume-netshare --basedir=/srv nfs -o port=2049,nolock,proto=tcp,nfsvers=3 --version=3
