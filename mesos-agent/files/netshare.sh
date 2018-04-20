a61354a51071d3c94ec7d82ddf98cc292a2e457c
Stéphane Cottin <stephane.cottin@vixns.com>
2018-04-20 09:51:31 +0200

    updates

--- mesos-agent/files/netshare.sh
+++ mesos-agent/files/netshare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 exec 1> >(exec logger -p user.info -t "$(basename $(pwd))[$(cat $(pwd)/supervise/pid)]")
 exec 2> >(exec logger -p user.err -t "$(basename $(pwd))[$(cat $(pwd)/supervise/pid)]")
+export SOURCE_MAP_FILE=/etc/netshare.map
+[ -e $SOURCE_MAP_FILE ] || touch $SOURCE_MAP_FILE
 exec /usr/bin/docker-volume-netshare --basedir=/srv nfs -o port=2049,nolock,proto=tcp,nfsvers=3 --version=3
